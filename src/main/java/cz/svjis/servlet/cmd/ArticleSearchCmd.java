222a36d9a2b216f06c53c7def8a27b4577e13341
Jaroslav Beran <jaroslav.beran@gmail.com>
2019-12-15 15:04:46 +0100

    Top 5 articles for limited period, fixes #2

--- src/main/java/cz/svjis/servlet/cmd/ArticleSearchCmd.java
+++ src/main/java/cz/svjis/servlet/cmd/ArticleSearchCmd.java
@@ -73,7 +73,9 @@ public class ArticleSearchCmd extends Command {
                 Integer.valueOf(getSetup().getProperty("article.page.size")),
                 true, false);
         getRequest().setAttribute("articleList", articleList);
-        ArrayList<Article> articleTopList = articleDao.getArticleTopList(getUser(), Integer.valueOf(getSetup().getProperty("article.top.size")));
+        ArrayList<Article> articleTopList = articleDao.getArticleTopList(getUser(), 
+                Integer.valueOf(getSetup().getProperty("article.top.size")),
+                (getSetup().getProperty("article.top.months") != null) ? Integer.valueOf(getSetup().getProperty("article.top.months")) : 12);
         getRequest().setAttribute("articleTopList", articleTopList);
         ArticleListInfo articleListInfo = new ArticleListInfo();
         articleListInfo.setNumOfArticles(articleDao.getNumOfArticlesFromSearch(parSearch, getUser(),
