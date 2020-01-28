d4dcd797fdbf71d1a768cf4fb6e299b5241c216f
Jaroslav Beran <jaroslav.beran@gmail.com>
2020-01-28 21:12:37 +0100

    Fault comment notification

--- src/main/java/cz/svjis/servlet/cmd/FaultReportingInsertCommentCmd.java
+++ src/main/java/cz/svjis/servlet/cmd/FaultReportingInsertCommentCmd.java
@@ -8,8 +8,12 @@ package cz.svjis.servlet.cmd;
 import cz.svjis.bean.FaultReport;
 import cz.svjis.bean.FaultReportComment;
 import cz.svjis.bean.FaultReportDAO;
+import cz.svjis.bean.MailDAO;
+import cz.svjis.bean.User;
 import cz.svjis.servlet.CmdContext;
+import cz.svjis.servlet.Command;
 import cz.svjis.validator.Validator;
+import java.util.ArrayList;
 import java.util.Date;
 import javax.servlet.RequestDispatcher;
 
@@ -17,7 +21,7 @@ import javax.servlet.RequestDispatcher;
  *
  * @author jarberan
  */
-public class FaultReportingInsertCommentCmd extends FaultAbstractCmd {
+public class FaultReportingInsertCommentCmd extends Command {
     
     public FaultReportingInsertCommentCmd(CmdContext ctx) {
         super(ctx);
@@ -47,7 +51,26 @@ public class FaultReportingInsertCommentCmd extends FaultAbstractCmd {
 
             faultDao.setUserWatchingFaultReport(report.getId(), getUser().getId());
 
-            sendNotification(report, "mail.template.fault.comment.notification", faultDao.getUserListForNotificationAboutNewComment(report.getId()));
+            // send notification
+            String subject = getCompany().getInternetDomain() + ": #" + report.getId() + " - " + report.getSubject() + " (New comment)";
+            String tBody = getSetup().getProperty("mail.template.fault.comment.notification");
+            MailDAO mailDao = new MailDAO(
+                    getCnn(),
+                    getSetup().getProperty("mail.smtp"),
+                    getSetup().getProperty("mail.login"),
+                    getSetup().getProperty("mail.password"),
+                    getSetup().getProperty("mail.sender"));
+            ArrayList<User> userList = faultDao.getUserListForNotificationAboutNewComment(report.getId());
+            for (User u : userList) {
+                if (u.getId() == getUser().getId()) {
+                    continue;
+                }
+                String body = String.format(tBody,
+                        getUser().getFirstName() + " " + getUser().getLastName(),
+                        "<a href=\"http://" + getCompany().getInternetDomain() + "/Dispatcher?page=faultDetail&id=" + report.getId() + "\">#" + report.getId() + " - " + report.getSubject() + "</a>",
+                        c.getBody().replace("\n", "<br>"));
+                mailDao.queueMail(getCompany().getId(), u.geteMail(), subject, body);
+            }
         }
         
         String url = "Dispatcher?page=faultDetail&id=" + parId;
