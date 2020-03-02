42bec6a1a053fa8c011955289dc061b0898202fc
Jaroslav Beran <jaroslav.beran@gmail.com>
2020-03-03 00:09:34 +0100

    BadPageCmd package fix

--- src/main/java/cz/svjis/servlet/cmd/BadPageCmd.java
+++ src/main/java/cz/svjis/servlet/cmd/BadPageCmd.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package cz.svjis.bean;
+package cz.svjis.servlet.cmd;
 
 import cz.svjis.servlet.CmdContext;
 import cz.svjis.servlet.Command;
