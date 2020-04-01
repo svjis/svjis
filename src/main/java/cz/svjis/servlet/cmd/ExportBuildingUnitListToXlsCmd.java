1962173f2c47aee66d5725799bc0a748a78c18fa
Jaroslav Beran <jaroslav.beran@gmail.com>
2020-04-01 21:05:24 +0200

    Formatting

--- src/main/java/cz/svjis/servlet/cmd/ExportBuildingUnitListToXlsCmd.java
+++ src/main/java/cz/svjis/servlet/cmd/ExportBuildingUnitListToXlsCmd.java
@@ -17,6 +17,10 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -40,37 +44,54 @@ public class ExportBuildingUnitListToXlsCmd extends Command {
             getResponse().setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
             getResponse().setHeader("Content-Disposition", "attachment; filename=BuildingUnitList_" + sdf.format(new Date()) + ".xlsx");
             outb = getResponse().getOutputStream();
-            try (XSSFWorkbook workbook = new XSSFWorkbook()) {
-                XSSFSheet sheet = workbook.createSheet("Building units");
 
-                LanguageDAO languageDao = new LanguageDAO(getCnn());
-                Language lang = languageDao.getDictionary(getUser().getLanguageId());
-                BuildingDAO buildingDao = new BuildingDAO(getCnn());
-                ArrayList<BuildingUnit> buildingUnitList = new ArrayList(buildingDao.getBuildingUnitList(
-                        buildingDao.getBuilding(getUser().getCompanyId()).getId(),
-                        0));
+            LanguageDAO languageDao = new LanguageDAO(getCnn());
+            Language lang = languageDao.getDictionary(getUser().getLanguageId());
+            BuildingDAO buildingDao = new BuildingDAO(getCnn());
+            ArrayList<BuildingUnit> buildingUnitList = new ArrayList(buildingDao.getBuildingUnitList(
+                    buildingDao.getBuilding(getUser().getCompanyId()).getId(),
+                    0));
+
+            try (XSSFWorkbook workbook = new XSSFWorkbook()) {
+                XSSFSheet sheet = workbook.createSheet(lang.getText("Building unit list"));
 
                 int l = 0;
                 int c = 0;
 
                 //-- Header
+                Font font = workbook.createFont();
+                font.setBold(true);
+                CellStyle style1 = workbook.createCellStyle();
+                style1.setFont(font);
+                CellStyle style2 = workbook.createCellStyle();
+                style2.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
+                style2.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+                style2.setFont(font);
+
                 Row row = sheet.createRow(l++);
                 Cell cell = row.createCell(c++);
+                cell.setCellStyle(style1);
                 cell.setCellValue(lang.getText("Building unit list"));
                 l++;
                 row = sheet.createRow(l++);
                 c = 0;
                 cell = row.createCell(c++);
+                cell.setCellStyle(style2);
                 cell.setCellValue(lang.getText("Type"));
                 cell = row.createCell(c++);
+                cell.setCellStyle(style2);
                 cell.setCellValue(lang.getText("Registration Id."));
                 cell = row.createCell(c++);
+                cell.setCellStyle(style2);
                 cell.setCellValue(lang.getText("Description"));
                 cell = row.createCell(c++);
+                cell.setCellStyle(style2);
                 cell.setCellValue(lang.getText("Owner list"));
                 cell = row.createCell(c++);
+                cell.setCellStyle(style2);
                 cell.setCellValue(lang.getText("Numerator"));
                 cell = row.createCell(c++);
+                cell.setCellStyle(style2);
                 cell.setCellValue(lang.getText("Denominator"));
 
                 for (BuildingUnit u: buildingUnitList) {
@@ -97,6 +118,12 @@ public class ExportBuildingUnitListToXlsCmd extends Command {
                         cell.setCellValue(String.valueOf(owner.getSalutation() + " " + owner.getFirstName() + " " + owner.getLastName()).trim());
                     }
                 }
+                sheet.autoSizeColumn(0);
+                sheet.autoSizeColumn(1);
+                sheet.autoSizeColumn(2);
+                sheet.autoSizeColumn(3);
+                sheet.autoSizeColumn(4);
+                sheet.autoSizeColumn(5);
                 workbook.write(outb);
             }
         } finally {
--- src/main/java/cz/svjis/servlet/cmd/ExportUserListToXlsCmd.java
+++ src/main/java/cz/svjis/servlet/cmd/ExportUserListToXlsCmd.java
@@ -16,6 +16,10 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -40,49 +44,73 @@ public class ExportUserListToXlsCmd extends Command {
             getResponse().setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
             getResponse().setHeader("Content-Disposition", "attachment; filename=UserList_" + sdf.format(new Date()) + ".xlsx");
             outb = getResponse().getOutputStream();
+
+            LanguageDAO languageDao = new LanguageDAO(getCnn());
+            Language lang = languageDao.getDictionary(getUser().getLanguageId());
+            UserDAO userDao = new UserDAO(getCnn());
+            ArrayList<User> userList = new ArrayList(userDao.getUserList(getUser().getCompanyId(), false, 0, true));
+
             try (XSSFWorkbook workbook = new XSSFWorkbook()) {
-                XSSFSheet sheet = workbook.createSheet("Users");
-                
-                LanguageDAO languageDao = new LanguageDAO(getCnn());
-                Language lang = languageDao.getDictionary(getUser().getLanguageId());
-                UserDAO userDao = new UserDAO(getCnn());
-                ArrayList<User> userList = new ArrayList(userDao.getUserList(getUser().getCompanyId(), false, 0, true));
+                XSSFSheet sheet = workbook.createSheet(lang.getText("User list"));
                 
                 int l = 0;
                 int c = 0;
                 
                 //-- Header
+                Font font = workbook.createFont();
+                font.setBold(true);
+                CellStyle style1 = workbook.createCellStyle();
+                style1.setFont(font);
+                CellStyle style2 = workbook.createCellStyle();
+                style2.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
+                style2.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+                style2.setFont(font);
+
                 Row row = sheet.createRow(l++);
                 Cell cell = row.createCell(c++);
+                cell.setCellStyle(style1);
                 cell.setCellValue(lang.getText("User list"));
                 l++;
                 row = sheet.createRow(l++);
                 c = 0;
                 cell = row.createCell(c++);
+                cell.setCellStyle(style2);
                 cell.setCellValue(lang.getText("Salutation"));
                 cell = row.createCell(c++);
+                cell.setCellStyle(style2);
                 cell.setCellValue(lang.getText("First name"));
                 cell = row.createCell(c++);
+                cell.setCellStyle(style2);
                 cell.setCellValue(lang.getText("Last name"));
                 cell = row.createCell(c++);
+                cell.setCellStyle(style2);
                 cell.setCellValue(lang.getText("Language"));
                 cell = row.createCell(c++);
+                cell.setCellStyle(style2);
                 cell.setCellValue(lang.getText("Address"));
                 cell = row.createCell(c++);
+                cell.setCellStyle(style2);
                 cell.setCellValue(lang.getText("City"));
                 cell = row.createCell(c++);
+                cell.setCellStyle(style2);
                 cell.setCellValue(lang.getText("Post code"));
                 cell = row.createCell(c++);
+                cell.setCellStyle(style2);
                 cell.setCellValue(lang.getText("Country"));
                 cell = row.createCell(c++);
+                cell.setCellStyle(style2);
                 cell.setCellValue(lang.getText("Fixed phone"));
                 cell = row.createCell(c++);
+                cell.setCellStyle(style2);
                 cell.setCellValue(lang.getText("Cell phone"));
                 cell = row.createCell(c++);
+                cell.setCellStyle(style2);
                 cell.setCellValue(lang.getText("E-Mail"));
                 cell = row.createCell(c++);
+                cell.setCellStyle(style2);
                 cell.setCellValue(lang.getText("Enabled"));
                 cell = row.createCell(c++);
+                cell.setCellStyle(style2);
                 cell.setCellValue(lang.getText("Last login"));
                 
                 for (User u: userList) {
@@ -120,6 +148,19 @@ public class ExportUserListToXlsCmd extends Command {
                         c++;
                     }
                 }
+                sheet.autoSizeColumn(0);
+                sheet.autoSizeColumn(1);
+                sheet.autoSizeColumn(2);
+                sheet.autoSizeColumn(3);
+                sheet.autoSizeColumn(4);
+                sheet.autoSizeColumn(5);
+                sheet.autoSizeColumn(6);
+                sheet.autoSizeColumn(7);
+                sheet.autoSizeColumn(8);
+                sheet.autoSizeColumn(9);
+                sheet.autoSizeColumn(10);
+                sheet.autoSizeColumn(11);
+                sheet.autoSizeColumn(12);
                 workbook.write(outb);
             }
         } finally {
