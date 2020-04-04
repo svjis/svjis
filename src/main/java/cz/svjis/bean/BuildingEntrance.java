91c4c333aa4704cbab1f4e1d7c2b3b7871412f93
Jaroslav Beran <jaroslav.beran@gmail.com>
2020-04-04 21:10:00 +0200

    Building Entrance DAO

---
+++ src/main/java/cz/svjis/bean/BuildingEntrance.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.svjis.bean;
+
+/**
+ *
+ * @author jarberan
+ */
+public class BuildingEntrance {
+    private int id;
+    private int buildingId;
+    private String description;
+    private String address;
+
+    public BuildingEntrance() {
+        clean();
+    }
+    
+    public void clean() {
+        id = 0;
+        buildingId = 0;
+        description = "";
+        address = "";
+    }
+    
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the buildingId
+     */
+    public int getBuildingId() {
+        return buildingId;
+    }
+
+    /**
+     * @param buildingId the buildingId to set
+     */
+    public void setBuildingId(int buildingId) {
+        this.buildingId = buildingId;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+}
