c1e0e0fb0f6a58ed6601d5762b630cf0750b7288
Jaroslav Beran <jaroslav.beran@gmail.com>
2020-05-03 14:56:05 +0200

    Correction

--- src/main/webapp/js/Redaction_InquiryEdit.js
+++ src/main/webapp/js/Redaction_InquiryEdit.js
@@ -1,12 +1,12 @@
 "use strict";
 
-var noDelete = getLastOptionNo();
-var desc = document.getElementsByTagName('th')[0].innerHTML;
+let noDelete = getLastOptionNo();
+let desc = document.getElementById('opt_parent').getElementsByTagName('th')[0].innerHTML;
 
 
 function getLastOptionNo() {
-    var i = 0;
-    var p = null;
+    let i = 0;
+    let p = null;
 
     do {
         i++;
@@ -17,34 +17,34 @@ function getLastOptionNo() {
 }
 
 function addOption() {
-    var lastOptionNo = getLastOptionNo();
+    let lastOptionNo = getLastOptionNo();
     lastOptionNo++;
-    var html =  '    <th scope="row" style="text-align: left">' + desc.replace(1, lastOptionNo) + '</th>\n' +
+    let html =  '    <th scope="row" style="text-align: left">' + desc.replace(1, lastOptionNo) + '</th>\n' +
                 '    <td>\n' +
                 '        <input type="hidden" name="oid_' + lastOptionNo + '" value="0">\n' +
                 '        <input id="common-input" type="text" name="o_' + lastOptionNo + '" size="50" maxlength="250" value="">\n' +
                 '    </td>\n' +
                 '    <td>&nbsp;</td>\n';
 
-    var p = document.getElementById('opt_parent');
-    var newElement = document.createElement('tr');
+    let p = document.getElementById('opt_parent');
+    let newElement = document.createElement('tr');
     newElement.setAttribute('id', 'opt_' + lastOptionNo);
     newElement.innerHTML = html;
     p.appendChild(newElement);
 
-    var button = document.getElementById('remove-option');
+    let button = document.getElementById('remove-option');
     button.disabled = false;
 }
 
 function removeOption() {
-    var lastOptionNo = getLastOptionNo();
+    let lastOptionNo = getLastOptionNo();
 
     if (lastOptionNo !== noDelete) {
-        var element = document.getElementById('opt_' + lastOptionNo);
+        let element = document.getElementById('opt_' + lastOptionNo);
         element.parentNode.removeChild(element);
     }
     if (noDelete === (lastOptionNo - 1)) {
-        var button = document.getElementById('remove-option');
+        let button = document.getElementById('remove-option');
         button.disabled = true;
     }
 }
