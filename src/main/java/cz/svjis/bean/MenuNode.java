b748c6951a0f5fab40531e860ff4fc349fa7610f
Jaroslav Beran <jaroslav.beran@gmail.com>
2020-05-10 23:26:50 +0200

    File stamp

--- src/main/java/cz/svjis/bean/MenuNode.java
+++ src/main/java/cz/svjis/bean/MenuNode.java
@@ -1,5 +1,21 @@
+/*
+ *       MenuNode.java
+ *
+ *       This file is part of SVJIS project.
+ *       https://github.com/svjis/svjis
+ *
+ *       SVJIS is free software; you can redistribute it and/or modify
+ *       it under the terms of the GNU General Public License as published by
+ *       the Free Software Foundation; either version 3 of the License, or
+ *       (at your option) any later version. <http://www.gnu.org/licenses/>
+ */
+
 package cz.svjis.bean;
 
+/**
+ *
+ * @author jaroslav_b
+ */
 public class MenuNode {
     
     private int id;
--- src/main/java/cz/svjis/bean/Permission.java
+++ src/main/java/cz/svjis/bean/Permission.java
@@ -1,7 +1,15 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *       Permission.java
+ *
+ *       This file is part of SVJIS project.
+ *       https://github.com/svjis/svjis
+ *
+ *       SVJIS is free software; you can redistribute it and/or modify
+ *       it under the terms of the GNU General Public License as published by
+ *       the Free Software Foundation; either version 3 of the License, or
+ *       (at your option) any later version. <http://www.gnu.org/licenses/>
  */
+
 package cz.svjis.bean;
 
 /**
