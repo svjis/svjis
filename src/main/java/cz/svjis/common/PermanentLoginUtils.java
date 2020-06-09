9fd913b43c491dd8082f675603aea67e9ac1ac15
Jaroslav Beran <jaroslav.beran@gmail.com>
2020-06-09 21:39:29 +0200

    Cookies setSecure true

--- src/main/java/cz/svjis/common/PermanentLoginUtils.java
+++ src/main/java/cz/svjis/common/PermanentLoginUtils.java
@@ -126,6 +126,7 @@ public class PermanentLoginUtils {
         Cookie c = new Cookie(name, value);
         c.setMaxAge(expiry);
         c.setHttpOnly(true);
+        c.setSecure(true);
         return c;
     }
     
