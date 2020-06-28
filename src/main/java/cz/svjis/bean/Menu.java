347e3bd6288c2919b110a86676463658718cbc13
Jaroslav Beran <jaroslav.beran@gmail.com>
2020-06-28 10:56:29 +0200

    Refa writeOptions - StringBuilder

--- src/main/java/cz/svjis/bean/Menu.java
+++ src/main/java/cz/svjis/bean/Menu.java
@@ -140,12 +140,12 @@ public class Menu {
     
     public String writeOptions(List<MenuItem> menu, int level, int selected) {
         String ident = "&nbsp;&nbsp;&nbsp;";
-        String output = "";
+        StringBuilder output = new StringBuilder(); 
 
         for (MenuItem m: menu) {
-            String idn = "";
+            StringBuilder idn = new StringBuilder(); 
             for (int n = 0; n < level; n++) {
-                idn += ident;
+                idn.append(ident);
             }
 
             String sel;
@@ -155,12 +155,12 @@ public class Menu {
                 sel = "";
             }
 
-            output += String.format("<option value=\"%d\" %s>%s</option>\n", m.getSection().getId(), sel, idn + m.getSection().getDescription());
+            output.append(String.format("<option value=\"%d\" %s>%s</option>%n", m.getSection().getId(), sel, idn + m.getSection().getDescription()));
 
             if ((m.getSubSections() != null) && (!m.getSubSections().isEmpty()))
-                output += writeOptions(m.getSubSections(), level + 1, selected);
+                output.append(writeOptions(m.getSubSections(), level + 1, selected));
         }
-        return output;
+        return output.toString();
     }
 
     /**
