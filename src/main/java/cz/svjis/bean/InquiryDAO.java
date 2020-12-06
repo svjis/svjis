e34913a159ea292a7aa333e9a3128bba8a9133d4
Jaroslav Beran <jaroslav.beran@gmail.com>
2020-12-06 12:27:42 +0100

    Unused import removed

--- src/main/java/cz/svjis/bean/InquiryDAO.java
+++ src/main/java/cz/svjis/bean/InquiryDAO.java
@@ -17,7 +17,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
